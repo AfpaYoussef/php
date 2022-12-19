91de2670fbdf135dd37ba94e0edbc840a2ce4a65
AfpaYoussef <youssefchaibi@hotmail.com>
2022-12-19 16:50:52 +0100

    Première publication

---
+++ Les_tableaux/exercice2.php
@@ -0,0 +1,91 @@
+<html>
+<body bgcolor=darkgoldenrod>
+<br><br><br><br>
+
+<table align=center border=1>
+<tr align=center>
+  <td bgcolor=sandybrown> 
+    <b>Pays</b>
+  </td>
+  <td bgcolor=gold> 
+     <b>Capitales</b> 
+  </td>
+</tr>
+
+
+<?php 
+$capitales = array(
+    "Bucarest" => "Roumanie",
+    "Bruxelles" => "Belgique",
+    "Oslo" => "Norvège",
+    "Ottawa" => "Canada",
+    "Paris" => "France",
+    "Port-au-Prince" => "Haïti",
+    "Port-d'Espagne" => "Trinité-et-Tobago",
+    "Prague" => "République tchèque",
+    "Rabat" => "Maroc",
+    "Riga" => "Lettonie",
+    "Rome" => "Italie",
+    "San José" => "Costa Rica",
+    "Santiago" => "Chili",
+    "Sofia" => "Bulgarie",
+    "Alger" => "Algérie",
+    "Amsterdam" => "Pays-Bas",
+    "Andorre-la-Vieille" => "Andorre",
+    "Asuncion" => "Paraguay",
+    "Athènes" => "Grèce",
+    "Bagdad" => "Irak",
+    "Bamako" => "Mali",
+    "Berlin" => "Allemagne",
+    "Bogota" => "Colombie",
+    "Brasilia" => "Brésil",
+    "Bratislava" => "Slovaquie",
+    "Varsovie" => "Pologne",
+    "Budapest" => "Hongrie",
+    "Le Caire" => "Egypte",
+    "Canberra" => "Australie",
+    "Caracas" => "Venezuela",
+    "Jakarta" => "Indonésie",
+    "Kiev" => "Ukraine",
+    "Kigali" => "Rwanda",
+    "Kingston" => "Jamaïque",
+    "Lima" => "Pérou",
+    "Londres" => "Royaume-Uni",
+    "Madrid" => "Espagne",
+    "Malé" => "Maldives",
+    "Mexico" => "Mexique",
+    "Minsk" => "Biélorussie",
+    "Moscou" => "Russie",
+    "Nairobi" => "Kenya",
+    "New Delhi" => "Inde",
+    "Stockholm" => "Suède",
+    "Téhéran" => "Iran",
+    "Tokyo" => "Japon",
+    "Tunis" => "Tunisie",
+    "Copenhague" => "Danemark",
+    "Dakar" => "Sénégal",
+    "Damas" => "Syrie",
+    "Dublin" => "Irlande",
+    "Erevan" => "Arménie",
+    "La Havane" => "Cuba",
+    "Helsinki" => "Finlande",
+    "Islamabad" => "Pakistan",
+    "Vienne" => "Autriche",
+    "Vilnius" => "Lituanie",
+    "Zagreb" => "Croatie"
+);
+asort($capitales);
+
+foreach ($capitales as $pays => $nomcapitale)
+{
+  echo "<tr align=center>";
+  echo "<td bgcolor=sandybrown> $nomcapitale  </td>";
+  echo "<td bgcolor=gold>  $pays </td>";
+  echo "</tr>";
+}
+
+
+?>
+</table> 
+</body>
+</html>

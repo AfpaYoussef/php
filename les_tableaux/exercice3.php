c10110aabd4d2c1409f91bcad210ef0e9bbd1a86
AfpaYoussef <youssefchaibi@hotmail.com>
2022-12-21 11:23:22 +0100

    exercice3 fini

--- les_tableaux/exercice3.php
+++ les_tableaux/exercice3.php
@@ -18,6 +18,32 @@ $departements = array(
     "Normandie" => array("Calvados", "Eure", "Manche", "Orne", "Seine-Maritime")
 );
 
+// Départements par ordre alphabétique
+ksort($departements);
+
+foreach($departements as $region => $depart){
+    echo 'La région ' .$region. ' comprend les départements : <br><br>';
+
+    foreach( $depart as $key ){
+    echo ' ' . $key. '<br>';
+}
+echo '<br>';
+}
+?>
+
+
+<br>
+
+
+<?php 
+
+$departements = array(
+    "Hauts-de-france" => array("Aisne", "Nord", "Oise", "Pas-de-Calais", "Somme"),
+    "Bretagne" => array("Côtes d'Armor", "Finistère", "Ille-et-Vilaine", "Morbihan"),
+    "Grand-Est" => array("Ardennes", "Aube", "Marne", "Haute-Marne", "Meurthe-et-Moselle", "Meuse", "Moselle", "Bas-Rhin", "Haut-Rhin", "Vosges"),
+    "Normandie" => array("Calvados", "Eure", "Manche", "Orne", "Seine-Maritime")
+);
+
 foreach ($departements as $region => $depart){ 
 
 echo $region.":".count($depart)."<br>";

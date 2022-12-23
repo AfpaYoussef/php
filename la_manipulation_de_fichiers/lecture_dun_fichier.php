8821b3101a0320d918f38eef72f40e4413fc7b45
AfpaYoussef <youssefchaibi@hotmail.com>
2022-12-23 12:20:58 +0100

    commit

---
+++ la_manipulation_de_fichiers/lecture_dun_fichier.php
@@ -0,0 +1,9 @@
+<!-- Écrire un programme qui lit ce fichier et qui construit une page web contenant une liste de liens hypertextes.
+Utilisez la fonction file() qui permet de lire directement un fichier et renvoie son contenu sous forme de tableau.-->
+
+<?php
+    $l=file(lien.text);
+    echo $l[0];
+
+
+?>

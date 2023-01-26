d3eb68656425502e7e19ed0217cf412ecf658aa0
AfpaYoussef <youssefchaibi@hotmail.com>
2023-01-26 14:32:16 +0100

    commit(amélioration)

--- authentification_et_motdepasse/redirection.php
+++ authentification_et_motdepasse/redirection.php
@@ -15,17 +15,24 @@ header('Refresh: '.$delai.';url='.$url);
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style1.css" media="screen" type="text/css" />
     <title>Redirection en HTML</title>
 </head>
 <body>
 
-    <p>Félicitations! Nous sommes heureux de vous compter parmi nos membres.</p>
+
+<style>
+    .heading { color:#FFFF00; }
+  </style>
+
+
+    <div align= center><span class="blink_me"><p class="heading">Félicitations! Nous sommes heureux de vous compter parmi nos membres.</p>
         
-    <p>Vous allez être redirigé(e) vers la page de connexion du site dans quelques instants.</p>
+    <p class="heading">Vous allez être redirigé(e) vers la page de connexion du site dans quelques instants.</p>
 
-    <p>Il y aura plein de lots à gagner: de l'argent, des voitures,... </p>
+    <p class="heading">Il y aura plein de lots à gagner: de l'argent, des voitures,... </p>
 
-    <p>Vous visiterez notre site avec plein de rêves en tête.</p>
+    <p class="heading">Vous visiterez notre site avec plein de rêves en tête.</p></span></div>
 
 </body>
 </html>
